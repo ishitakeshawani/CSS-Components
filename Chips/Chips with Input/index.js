84d67209c11e96b71efb0bc7a23ca9d5d12a1a6d
ishitakeshawani <keshwaniishita054@gmail.com>
2022-01-25 16:16:27 +0530

    added chips with input

---
+++ Chips/Chips with Input/index.js
@@ -0,0 +1,50 @@
+// function showList() {
+//   // console.log("hi");
+//   var list = document.getElementById("ollist");
+
+//   if (list.style.visibility == "hidden") {
+//     list.style.visibility = "visible";
+//   } else {
+//     list.style.visibility = "hidden";
+//   }
+// }
+
+// document.onclick = () => {
+//   if (document.getElementById("ollist").style.visibility == "visible") {
+//     document.getElementById("ollist").style.visibility = "hidden";
+//   }
+// };
+
+// function selectItem(a) {
+//   var x = a.innerHTML;
+//   // res.push(x);
+//   // console.log(res);
+//   var node = document.createElement("LI");
+//   var btn = document.createElement("button");
+//   btn.innerText = "x";
+//   node.append(x);
+//   node.appendChild(btn);
+//   btn.addEventListener("click", function (event) {
+//     event.target.parentElement.remove();
+//   });
+//   document.getElementById("demo").appendChild(node);
+// }
+
+function addInput(e) {
+  e.preventDefault();
+  var b = document.getElementById("chips").value;
+  var node = document.createElement("LI");
+  var btn = document.createElement("button");
+  btn.innerText = "x";
+  node.append(b);
+  node.appendChild(btn);
+  btn.addEventListener("click", function (event) {
+    event.target.parentElement.remove();
+  });
+  document.getElementById("demo").appendChild(node);
+  formik.reset();
+}
+
+const formik = document.querySelector(".chipform");
+
+formik.addEventListener("submit", addInput);
