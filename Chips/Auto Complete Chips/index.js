25526716d59cf1688a13bd7f828260c02487575f
ishitakeshawani <keshwaniishita054@gmail.com>
2022-01-25 15:24:11 +0530

    implemented input function in chips

--- Chips/Auto Complete Chips/index.js
+++ Chips/Auto Complete Chips/index.js
@@ -1,5 +1,5 @@
 function showList() {
-  console.log("hi");
+  // console.log("hi");
   var list = document.getElementById("ollist");
 
   if (list.style.visibility == "hidden") {
@@ -9,7 +9,6 @@ function showList() {
   }
 }
 
-var res = [];
 document.onclick = () => {
   if (document.getElementById("ollist").style.visibility == "visible") {
     document.getElementById("ollist").style.visibility = "hidden";
@@ -18,7 +17,7 @@ document.onclick = () => {
 
 function selectItem(a) {
   var x = a.innerHTML;
-  res.push(x);
+  // res.push(x);
   // console.log(res);
   var node = document.createElement("LI");
   var btn = document.createElement("button");
@@ -30,3 +29,22 @@ function selectItem(a) {
   });
   document.getElementById("demo").appendChild(node);
 }
+
+function addInput(e) {
+  e.preventDefault();
+  var b = document.getElementById("chips").value;
+  var node = document.createElement("LI");
+  var btn = document.createElement("button");
+  btn.innerText = "x";
+  node.append(b);
+  node.appendChild(btn);
+  btn.addEventListener("click", function (event) {
+    event.target.parentElement.remove();
+  });
+  document.getElementById("demo").appendChild(node);
+  formik.reset();
+}
+
+const formik = document.querySelector(".chipform");
+
+formik.addEventListener("submit", addInput);
